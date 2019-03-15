be9c79a3bcb4cdc541c75729d91b99208e0b6ed0
visualstudio <visualstudio@122-PC23>
2019-03-15 17:55:43 -0400

    second

--- c0730319 c0732152/Program.cs
+++ c0730319 c0732152/Program.cs
@@ -9,8 +9,72 @@ namespace c0730319_c0732152
     class Program
     {
         static void Main(string[] args)
+        { }
+      public class BankAccount
         {
-            int i = 5;
+            private string m_custonerName;
+
+            private double m_balance;
+
+            private bool m_frozen = false;
+
+            private BankAccount()
+            {
+            }
+
+            public BankAccount(string customerName, double balance)
+            {
+                m_custonerName = customerName;
+
+                m_balance = balance;
+            }
+            public string CustomerName
+            {
+                get { return m_custonerName; }
+            }
+
+            public double Balance
+            {
+                get { return m_balance; }
+            }
+            public void Debit(double amount)
+            {
+                if (m_frozen)
+                {
+                    throw new Exception(("Account frozen"));
+                }
+                if (amount > m_balance)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+                if (amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+
+                m_balance += amount;
+            }
+
+            public void Credit(double amount)
+            {
+                if (m_frozen)
+                {
+                    throw new Exception("Account frozen");
+                }
+                if (amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+
+                m_balance += amount;
+
+
+
+
+
+
+
+            }
 
         }
     }
