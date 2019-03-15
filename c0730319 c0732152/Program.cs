2d40d33181c9f4e6522d20000f81e305586cf4b8
visualstudio <visualstudio@122-PC23>
2019-03-15 17:57:42 -0400

    final commit

--- c0730319 c0732152/Program.cs
+++ c0730319 c0732152/Program.cs
@@ -4,78 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0730319_c0732152
+namespace C0724144_C0732134
 {
     class Program
     {
         static void Main(string[] args)
-        { }
-      public class BankAccount
         {
-            private string m_custonerName;
 
-            private double m_balance;
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
-            private bool m_frozen = false;
-
-            private BankAccount()
-            {
-            }
-
-            public BankAccount(string customerName, double balance)
-            {
-                m_custonerName = customerName;
-
-                m_balance = balance;
-            }
-            public string CustomerName
-            {
-                get { return m_custonerName; }
-            }
-
-            public double Balance
-            {
-                get { return m_balance; }
-            }
-            public void Debit(double amount)
-            {
-                if (m_frozen)
-                {
-                    throw new Exception(("Account frozen"));
-                }
-                if (amount > m_balance)
-                {
-                    throw new ArgumentOutOfRangeException("amount");
-                }
-                if (amount < 0)
-                {
-                    throw new ArgumentOutOfRangeException("amount");
-                }
-
-                m_balance += amount;
-            }
-
-            public void Credit(double amount)
-            {
-                if (m_frozen)
-                {
-                    throw new Exception("Account frozen");
-                }
-                if (amount < 0)
-                {
-                    throw new ArgumentOutOfRangeException("amount");
-                }
-
-                m_balance += amount;
-
-
-
-
-
-
-
-            }
-
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
+    }
+
+    public class BankAccount
+    {
+        private string m_custonerName;
+
+        private double m_balance;
+
+        private bool m_frozen = false;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_custonerName = customerName;
+
+            m_balance = balance;
+        }
+        public string CustomerName
+        {
+            get { return m_custonerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+        private void FreezoAccount()
+        {
+            m_frozen = true;
+        }
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
     }
 }
